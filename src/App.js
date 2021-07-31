2cf0ebf4c280c554298878670993cf6ffce4258f
Moroku <moroku96@gmail.com>
2021-07-31 13:58:43 +0200

    Working prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [size, setSize] = useState([3, 3]);
+  const [indexActive, setIndexActive] = useState(null);
+
+  const changeSize = (e) => {
+    let newSize = e.target.value <= 100 ? e.target.value : 100;
+    e.target.id === "rows" ? setSize([newSize, size[1]]) : setSize([size[0], newSize]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Converter</h1>
+      <input onChange={changeSize} type="text" id="rows" placeholder="Rows" />
+      <input onChange={changeSize} type="text" id="cols" placeholder="Cols" />
+      <MatrixVisualizer rows={size[0]} cols={size[1]} indexActive={indexActive} setIndexActive={setIndexActive} />
+      <ArrayVisualizer size={size[0] * size[1]} indexActive={indexActive} setIndexActive={setIndexActive} />
     </div>
   );
 }
 
-export default App;
+function MatrixVisualizer(props) {
+  return (
+    <table>
+      <tbody>
+        {generateArrayOfTrues(props.rows).map((_, index_row) => (
+          <tr key={index_row}>
+            {generateArrayOfTrues(props.cols).map((_, index_col) => <td key={index_col} className={index_row * props.cols + index_col === props.indexActive ? "on" : "off"} onClick={() => props.setIndexActive(index_row * props.cols + index_col)}></td>)}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+function ArrayVisualizer(props) {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          {generateArrayOfTrues(props.size).map((_, index) => <td key={index} className={index === props.indexActive ? "on" : "off"} onClick={() => props.setIndexActive(index)}></td>)}
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+function generateArrayOfTrues(size) {
+  let array = [];
+  for (let i = 0; i < size; i++) array.push(true);
+  return array;
+}
