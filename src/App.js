0f60cac9eb9998def8b91d0feb6140aaaa64a294
Moroku <moroku96@gmail.com>
2021-07-31 17:17:04 +0200

    Added basic style and polish

--- src/App.js
+++ src/App.js
@@ -2,48 +2,56 @@ import React, { useState } from 'react';
 import './App.css';
 
 export default function App() {
-  const [size, setSize] = useState([3, 3]);
+  const [initialRows, initialCols] = [4, 4]
+  const maxRowCol = 20
+
+  const [size, setSize] = useState([initialRows, initialCols]);
   const [indexActive, setIndexActive] = useState(null);
 
   const changeSize = (e) => {
-    let newSize = e.target.value <= 100 ? e.target.value : 100;
+    let newSize = e.target.value <= maxRowCol ? e.target.value : maxRowCol;
     e.target.id === "rows" ? setSize([newSize, size[1]]) : setSize([size[0], newSize]);
   }
 
   return (
     <div className="container">
-      <h1 className="title">Converter</h1>
-      <input onChange={changeSize} type="text" id="rows" placeholder="Rows" />
-      <input onChange={changeSize} type="text" id="cols" placeholder="Cols" />
+      <h1 className="title">Matrix - Array</h1>
+
+      <InputSize changeSize={changeSize} rowSize={size[0]} colSize={size[1]} />
+
       <MatrixVisualizer rows={size[0]} cols={size[1]} indexActive={indexActive} setIndexActive={setIndexActive} />
       <ArrayVisualizer size={size[0] * size[1]} indexActive={indexActive} setIndexActive={setIndexActive} />
     </div>
   );
 }
 
+function InputSize(props) {
+  return (
+    <div className="inputSizeContainer">
+      <input onChange={props.changeSize} type="text" id="rows" value={props.rowSize} className="input-size" />
+      <span className="inputMultiplicationChar"> × </span>
+      <input onChange={props.changeSize} type="text" id="cols" value={props.colSize} className="input-size" />
+    </div>
+  )
+}
+
 function MatrixVisualizer(props) {
   return (
-    <table>
-      <tbody>
-        {generateArrayOfTrues(props.rows).map((_, index_row) => (
-          <tr key={index_row}>
-            {generateArrayOfTrues(props.cols).map((_, index_col) => <td key={index_col} className={index_row * props.cols + index_col === props.indexActive ? "on" : "off"} onClick={() => props.setIndexActive(index_row * props.cols + index_col)}></td>)}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="matrix-container">
+      {generateArrayOfTrues(props.rows).map((_, index_row) => (
+        <div className="matrix-row" key={index_row}>
+          {generateArrayOfTrues(props.cols).map((_, index_col) => <div key={index_col} className={index_row * props.cols + index_col === props.indexActive ? "matrix-cell on" : "matrix-cell off"} onClick={() => props.setIndexActive(index_row * props.cols + index_col)}></div>)}
+        </div>
+      ))}
+    </div>
   )
 }
 
 function ArrayVisualizer(props) {
   return (
-    <table>
-      <tbody>
-        <tr>
-          {generateArrayOfTrues(props.size).map((_, index) => <td key={index} className={index === props.indexActive ? "on" : "off"} onClick={() => props.setIndexActive(index)}></td>)}
-        </tr>
-      </tbody>
-    </table>
+    <div className="array-container">
+      {generateArrayOfTrues(props.size).map((_, index) => <div key={index} className={index === props.indexActive ? "array-cell on" : "array-cell off"} onClick={() => props.setIndexActive(index)}></div>)}
+    </div>
   )
 }
 
